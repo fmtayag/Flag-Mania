d40a76fa40db98e83170df80addf1cd67fc7b1d4
MrNiceGT <franztayag@gmail.com>
2019-05-22 07:24:10 +0800

    add flags, update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import random
 import os
 pygame.init()
 
+WHITE = (255, 255, 255)
+BLACK = (25, 25, 25)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
 class Flag:
     def __init__(self, name):
         self.name = name
@@ -13,29 +19,23 @@ class Flag:
         self.image_rect.y = 25
 
 class Text:
-    def __init__(self, text, font, size, pos_y):
+    def __init__(self, text, font, size, pos_y, color = WHITE):
         self.text = text
         self.font = pygame.font.Font(os.path.join("fonts", f"{font}"), size)
-        self.label = self.font.render(text, False, WHITE)
+        self.label = self.font.render(text, False, color)
         self.label_rect = self.label.get_rect()
         self.label_rect.x = (screen_width / 2) - 150
         self.label_rect.y = pos_y
 
-button = pygame.Rect(100, 100, 50, 50)
-
-WHITE = (255, 255, 255)
-BLACK = (25, 25, 25)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
 score = 0
 question_number = 1
+correct_display_tick = 0
 ### INPUT MAXIMUM NUMBER OF QUESTIONS HERE >>>>>>>>>>
 max_questions = 30
 
 FPS = 60
 clock = pygame.time.Clock()
+correct_answer = ["hey now"]
 
 os.chdir("flags")
 flags = os.listdir()
@@ -43,7 +43,7 @@ print(flags)
 flags = [f.replace(".png", "") for f in flags]
 os.chdir("..")
 
-screen_width, screen_height = 400, 500
+screen_width, screen_height = 600, 400
 screen_resolution = (screen_width, screen_height)
 screen = pygame.display.set_mode(screen_resolution)
 pygame.display.set_caption("Vexill.io")
@@ -51,22 +51,27 @@ pygame.display.set_caption("Vexill.io")
 def game_loop():
     global score
     global question_number
-    random.shuffle(flags)
+    global correct_display_tick
+    global correct_answer
     random.shuffle(flags)
 
     choices = list()
     choice_labels = list()
     correct = list()
-
     for i in range(3):
         flag = Flag(flags[i])
         choices.append(flag)
 
+    ## Randomizes flag selection
     rng = random.randint(0, len(choices) - 1)
     
     running = True
     
     while running == True:
+        if correct_display_tick > 0: 
+            correct_display_tick -= 1
+        else:
+            pass
         clock.tick(FPS)
         mouse_pressed = False
         mouse_pos = pygame.mouse.get_pos()
@@ -83,7 +88,7 @@ def game_loop():
         choice_label3 = Text(choices[2].name, "Pixelart.ttf", 22, 250)
         score_label = Text(f"Score {score}", "Pixelart.ttf", 16, 300)
         number_label = Text(f"Question {question_number} out of {max_questions}", "Pixelart.ttf", 16, 325)
-        #correct_label = Text(f"Answer is {choices[rng].name}", "Pixelart.ttf", 16, 350)
+        correct_label = Text(f"Correct Answer is {correct_answer[0]}", "Pixelart.ttf", 16, screen_height - 30, GREEN)
         gameover_label = Text("Game Over!", "Pixelart.ttf", 32, screen_height - 30)
         
         choices_list = (choice_label1, choice_label2, choice_label3)
@@ -96,10 +101,16 @@ def game_loop():
             elif mouse_pressed == True and choices_list[i].label_rect.collidepoint(mouse_pos) and choices_list[i].text != choices[rng].name:
                 if score == 0:
                     question_number += 1
+                    correct_display_tick = 200
+                    del correct_answer[0]
+                    correct_answer.append(choices[rng].name)
                     game_loop()
                 elif score > 0:
                     score -= 1
                     question_number += 1
+                    correct_display_tick = 200
+                    del correct_answer[0]
+                    correct_answer.append(choices[rng].name)
                     game_loop()
  
         if question_number > max_questions:
@@ -114,6 +125,9 @@ def game_loop():
         screen.blit(choice_label3.label, choice_label3.label_rect)
         screen.blit(score_label.label, score_label.label_rect)
         screen.blit(number_label.label, number_label.label_rect)
+
+        if correct_display_tick > 0:
+            screen.blit(correct_label.label, correct_label.label_rect)
         
         pygame.display.update()
 game_loop()
